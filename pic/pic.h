95d9ff350c6c395c08bac241a8a29a1d4618c868
Madushyam <madushyam2006@example.com>
2026-04-20 17:10:11 +0530

    fix: haris change

--- pic/pic.h
+++ pic/pic.h
@@ -7,5 +7,7 @@ void pic_init();
 void pic_send_eoi(uint8_t irq);
 void pic_mask_irq(uint8_t irq);
 void pic_unmask_irq(uint8_t irq);
+void pic_mask_all();   // New: Masks all IRQs
+void pic_unmask_all(); // New: Unmasks all IRQs
 
 #endif
